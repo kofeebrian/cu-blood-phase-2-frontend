60dfee356bda0961570c00e783b6eb12f9ea67b5
KoFeeBriAN <coffeetk2017@gmail.com>
2019-02-05 13:51:53 +0700

    Doing Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import "../css/tailwind.css";
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "./Header";
+
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className='container mx-auto'>
 				<BrowserRouter>
 					<div>
+						<Header />
 						<Route path='/' component='' />
 					</div>
 				</BrowserRouter>
