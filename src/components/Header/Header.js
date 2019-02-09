ab0ff27458f04d6305e80d132786a05bb33b8441
Teeradham <5batman400@gmail.com>
2019-02-09 22:57:49 +0700

    Replace tailwind header with semantic header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 import Item from "./Item";
 
-class Header extends React.Component {
+class HeaderReact extends React.Component {
   componentDidMount() {}
 
   render() {
     return (
-      <nav className="ui container">
-        <div className="lg:flex flex-no-shrink items-stretch h-12">
-          <Link
-            to="#"
-            className="py-2 px-4 leading-normal text-white text-xl font-bold no-underline flex items-center"
-          >
-            CU BLOOD
-          </Link>
-          <div className="flex">
-            <Item to="/" title="Home" />
-            <Item to="/qr" title="QR reader" />
-            <Item to="/" title="Manage Staff" />
-            <Item to="/regis" title="Register" />
-          </div>
-        </div>
-        <div className="lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
-          <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
-            <Link
-              to="#"
-              className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center "
-            >
-              User Name
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <Menu inverted color={"red"}>
+        <Menu.Item header className="ui text centered width 10px">
+          CU Blood
+        </Menu.Item>
+        <Menu.Item className="ui borderless" name="Home" as={Link} to="/" />
+        <Menu.Item
+          className="ui borderless"
+          name="QR reader"
+          as={Link}
+          to="/qr"
+        />
+      </Menu>
     );
   }
 }
 
-export default connect(null)(Header);
+export default connect(null)(HeaderReact);
