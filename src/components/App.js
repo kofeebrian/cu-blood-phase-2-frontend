a6f5d23be569564b69dc75c7e5312f78d05723e5
CeylonT <nitikub@gmail.com>
2019-02-09 16:46:32 +0700

    Add Signup.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,23 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import QRReader from "./QRReader/QRReader";
+import Signup from "./Signup";
 
 class App extends Component {
-	render() {
-		return (
-			<div className='container mx-auto'>
-				<BrowserRouter>
-					<div>
-						<Header />
-						<Route path='/' exact component={Home} />
-						<Route path='/qr' exact component={QRReader} />
-					</div>
-				</BrowserRouter>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container mx-auto">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/" exact component={Home} />
+            <Route path="/qr" exact component={QRReader} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
