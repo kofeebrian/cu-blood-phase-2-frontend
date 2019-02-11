f783c4e799155f5aa0da34ae8b2820cb6e26ea7d
Teeradham <5batman400@gmail.com>
2019-02-11 10:54:27 +0700

    Add user menu on left side, add manage staff

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import { Menu, Icon, Dropdown } from "semantic-ui-react";
 
 import Item from "./Item";
 
@@ -10,17 +10,47 @@ class Header extends React.Component {
 
   render() {
     return (
-      <Menu inverted color={"red"}>
+      <Menu stackable inverted color={"red"}>
         <Menu.Item header className="ui text centered width 10px">
           CU Blood
         </Menu.Item>
+
         <Menu.Item className="ui borderless" name="Home" as={Link} to="/" />
+
         <Menu.Item
           className="ui borderless"
           name="QR reader"
           as={Link}
           to="/qr"
         />
+
+        <Menu.Item
+          className="ui borderless"
+          name="Manage staff"
+          as={Link}
+          to="/"
+        />
+
+        <Menu.Menu header position="right">
+          <Dropdown item pointing text="username">
+            <Dropdown.Menu>
+              <Dropdown.Item text="Edit profile" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+
+        {/*
+        <Menu.Item
+          className="ui borderless float right"
+          header
+          name="username"
+          as={Link}
+          to="/"
+        >
+          <Icon name="user" size="tiny" />
+          username
+        </Menu.Item>
+                */}
       </Menu>
     );
   }
