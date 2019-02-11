f0a25d883596abe1853ddc7c5df3dbe201b31df3
Teeradham <5batman400@gmail.com>
2019-02-11 10:30:35 +0700

    Change Home.js to function component, add icon to edit profile

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,36 +3,35 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Segment, Grid, Divider, Header, Icon } from "semantic-ui-react";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="ui container">
-        <Segment placeholder>
-          <Grid columns={3} divided stackable textAlign="center">
-            <Grid.Row verticalAlign="middle">
-              <Grid.Column container as={Link} to="/">
-                <Header>Manage profile</Header>
-              </Grid.Column>
+const Home = () => (
+  <div className="ui container p">
+    <Segment padded placeholder>
+      <Grid columns={3} divided stackable textAlign="center">
+        <Grid.Row verticalAlign="middle">
+          <Grid.Column container as={Link} to="/">
+            <Header icon>
+              <Icon color="red" name="user" size="huge" />
+              Edit profile
+            </Header>
+          </Grid.Column>
 
-              <Grid.Column container as={Link} to="/qr">
-                <Header icon>
-                  <Icon color="red" name="qrcode" size="huge" />
-                  QR Scanner
-                </Header>
-              </Grid.Column>
+          <Grid.Column container as={Link} to="/qr">
+            <Header icon>
+              <Icon color="red" name="qrcode" size="huge" />
+              QR Scanner
+            </Header>
+          </Grid.Column>
 
-              <Grid.Column container as={Link} to="/">
-                <Header icon>
-                  <Icon color="red" name="tasks" size="huge" />
-                  Manage staff
-                </Header>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-      </div>
-    );
-  }
-}
+          <Grid.Column container as={Link} to="/">
+            <Header icon>
+              <Icon color="red" name="tasks" size="huge" />
+              Manage staff
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  </div>
+);
 
 export default connect(null)(Home);
